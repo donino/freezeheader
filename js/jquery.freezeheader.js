e25cd5a47c288ca2ddf79a6d50bbfda979f46153
donino <dpardon@laposte.net>
2015-09-16 17:18:52 +0200

    Update jquery.freezeheader.js

--- js/jquery.freezeheader.js
+++ js/jquery.freezeheader.js
@@ -102,9 +102,9 @@ Version: 1.0.7
 	        $( window ).resize(function() {
 	             	params.normalizeHeader=false;
 	             	copiedHeader = false;
-	             	console.log("Removing freezed head:"+'#hd' + obj.id);
-	             	$('#hd' + obj.id).remove();                
-	            }
+	             	console.log("Removing freezed container:"+obj.container.attr('id'));
+	             	obj.container.remove();
+		});
         }
 
         function limiteAlcancado(obj, params) {
@@ -164,12 +164,7 @@ Version: 1.0.7
                 obj.container.css("position", "fixed");
             }
         }
-        $( window ).resize(function() {
-             	params.normalizeHeader=false;
-             	copiedHeader = false;
-             	console.log("Removing freezed container:"+obj.container.attr('id'));
-             	obj.container.remove();
-	});
+
         return this.each(function (i, e) {
             freezeHeader($(e));
         });
